484bd4a75eb74bc24956adcb1e913447f09cd753
Jimmy Ding <jimmyguding@gmail.com>
2023-12-04 11:22:49 -0400

    feat: modified day1

--- day01/main.hs
+++ day01/main.hs
@@ -1,18 +1,20 @@
 import Data.Bifunctor (first)
 import Data.List (find, isPrefixOf, isSuffixOf)
 
-ns :: [String]
-ns = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-
-nums :: [(String, Int)]
--- nums = zip (map show [0 .. 9]) [0 .. 9] -- part 1
-nums = zip ns [0 ..] ++ zip (map show [0 .. 9]) [0 .. 9]
+rn :: [(String, Int)]
+rn = zip (map show [0 .. 9]) [0 ..]
 
 match :: [(String, Int)] -> String -> Int
 match wd s = case find (flip isPrefixOf s . fst) wd of Just n -> snd n; Nothing -> match wd (drop 1 s)
 
-calc :: [String] -> Int
-calc l = sum (map (\s -> match nums s * 10 + match (map (first reverse) nums) (reverse s)) l)
+calc :: [(String, Int)] -> [String] -> Int
+calc n l = sum $ map (\s -> match n s * 10 + match (map (first reverse) n) (reverse s)) l
+
+part1 :: [String] -> Int
+part1 = calc rn
+
+part2 :: [String] -> Int
+part2 = calc $ zip ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] [0 ..] ++ rn
 
 main :: IO ()
-main = interact ((++ "\n") . show . calc . lines)
+main = interact $ (++ "\n") . show . part2 . lines
