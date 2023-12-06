c65ce7989f3196701e8a983909cde16b5e2542c4
Jimmy Ding <jimmyguding@gmail.com>
2023-12-06 02:37:15 -0400

    feat: refactor

--- day04/main.hs
+++ day04/main.hs
@@ -1,14 +1,14 @@
-import Data.List (intersect)
+import Data.List (elemIndex, intersect)
 
 parse :: String -> Int
-parse s = (\(x, y) f -> length ((f x :: [Int]) `intersect` f y)) (span ('|' /=) (dropWhile (':' /=) s)) (map read . words . drop 1)
-
-part1 :: [String] -> Int
-part1 = sum . map (flip div 2 . (2 ^) . parse)
+parse s = length $ uncurry intersect $ splitAt (sum $ elemIndex "|" $ words s) (words s)
 
 rec :: [Int] -> [String] -> Int
 rec lst [] = 0
 rec (h : t) (s : r) = h + rec [v + sum [h | idx < parse s] | (idx, v) <- zip [0 ..] t] r
+
+part1 :: [String] -> Int
+part1 = sum . map (flip div 2 . (2 ^) . parse)
 
 part2 :: [String] -> Int
 part2 s = rec (1 <$ [1 .. length s]) s
